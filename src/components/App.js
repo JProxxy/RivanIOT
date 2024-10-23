c3c512cf22093ae7d0dd0d26f58338b3fe4bb68a
Jhon Robert <nanimonanimonanimo@gmail.com>
2024-10-23 16:38:51 +0800

    6

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,7 @@ import ForgotPass from "./ForgotPass"; // Import ForgotPass component
 function App() {
   return (
     <Router> {/* Wrap everything in Router for routing */}
-      <div className="login">
-
-          <Routes>
-            <Route path="/" element={<LoginForm />} /> {/* Default route to LoginForm */}
-            <Route path="/signup" element={<SignUp />} /> {/* Route for SignUp */}
-            <Route path="/forgot-password" element={<ForgotPass />} /> {/* Route for ForgotPass */}
-          </Routes>
-        
-      </div>
+     
     </Router>
   );
 }
