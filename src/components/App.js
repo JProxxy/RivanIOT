fdccbba8985e5ccdad10df7db954ded893a8e125
Jhon Robert <nanimonanimonanimo@gmail.com>
2024-10-23 18:39:24 +0800

    9

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom'; // Ensure HashRouter is imported
+import { Routes, Route } from 'react-router-dom'; // Removed HashRouter import
 import LoginForm from "./LoginForm";
 import SignUp from "./SignUp"; 
 import ForgotPass from "./ForgotPass"; 
@@ -7,17 +7,15 @@ import "../css/App.css";
 
 function App() {
   return (
-    <HashRouter>
-      <div className="login">
-        <div className="backgroudndImg">
-          <Routes>
-            <Route path="/" element={<LoginForm />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/forgot-password" element={<ForgotPass />} />
-          </Routes>
-        </div>
+    <div className="login">
+      <div className="backgroudndImg">
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPass />} />
+        </Routes>
       </div>
-    </HashRouter>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; 
-import App from './components/App';
+import './index.css'; // Correct path to index.css
+import App from './components/App'; // Correct path to App component
 import reportWebVitals from './components/reportWebVitals';
-import { HashRouter } from 'react-router-dom'; // Import HashRouter
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter> {/* Wrap App with HashRouter */}
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
